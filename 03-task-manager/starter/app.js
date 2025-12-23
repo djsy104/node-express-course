c049572cd685a32552d26161b616933020a3ca52
Dj <148027370+djsy104@users.noreply.github.com>
2025-12-23 14:59:09 -0800

    Completed week5 HW

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const port = 3000;
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
