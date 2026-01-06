6f4be200a6f268e9c65bc6da94e7e9a036478aaa
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-01-06 14:33:28 -0800

    Finished week6 assignments

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
 // Middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // Routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
